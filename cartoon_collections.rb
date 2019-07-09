6660965ed1bb4a465910a61a241df9858d10baa6
Matthew Guzman <tomaffew@me.com>
2019-07-09 05:50:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|dwarf, i| puts " #{i+1}. #{dwarf}"}
+  array.each_with_index 
 end
 
 def summon_captain_planet(array)
-  array.map{|el| "#{el.capitalize}!"}  
+  array.map {|el| "#{el.capitalize}!"}  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length>4}
 end
 
 def find_the_cheese# code an argument here
